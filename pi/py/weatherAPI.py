ac45010db1f3c186a6a0f730eaabf0cdc559bb4a
AL <woodle.alex@gmail.com>
2018-09-15 09:18:56 -0400

    Updated weather API script

--- pi/py/weatherAPI.py
+++ pi/py/weatherAPI.py
@@ -7,25 +7,37 @@ lat = g.lat
 lng = g.lng
 
 
-api_latlng = 'http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=5ec3e53023c8502e481742440f9595de' % (lat, lng)
+api_latlng = 'http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=5ec3e$
 
 json_data = requests.get(api_latlng).json()
 
 location = json_data['name']
 wind = json_data['wind']['speed']
 kelvin = json_data['main']['temp']
+humidity = json_data['main']['humidity']
+pressure = json_data['main']['pressure']
 
+print
+print('OpenWeatherMap')
 print('Location: ' + location)
 print('Wind speed: ' + str(wind))
 print('Temperature: ' + str(pytemperature.k2f(float(kelvin))))
+print('Humidity: ' + str(humidity))
+print('Pressure: ' + str(pressure))
 
-darksky_api = 'https://api.darksky.net/forecast/36845a09793a4c98d39a8392d66fecc8/%s,%s' % (lat, lng)
+darksky_api = 'https://api.darksky.net/forecast/36845a09793a4c98d39a8392d66fecc8/%s,%s$
 json_darksky = requests.get(darksky_api).json()
 dark_location = json_darksky['timezone']
 dark_wind = json_darksky['currently']['windSpeed']
 dark_temperature = json_darksky['currently']['temperature']
-print()
+dark_summary = json_darksky['currently']['summary']
+
+print
 print("DarkSky")
 print('Location: ' + dark_location)
 print('Wind speed: ' + str(dark_wind))
 print('Temperature: ' + str(dark_temperature))
+print('Currently: ' + dark_summary)
+
+#print(api_latlng)
+#print(darksky_api)
