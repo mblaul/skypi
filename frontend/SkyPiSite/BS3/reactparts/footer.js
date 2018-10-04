992ccbeb4684b2c4612d926f05d7fb5e01496eff
Troy Butzin <tjbutzin@oakland.edu>
2018-10-03 20:29:38 -0400

    Separated footer and mainpanel from template

---
+++ frontend/SkyPiSite/BS3/reactparts/footer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer class="footer">
+            <div class="container-fluid">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+
+                    </ul>
+                </nav>
+                <p class="copyright pull-right">
+                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+                </p>
+            </div>
+        </footer>
+      </div>
+    )
+  }
+}
