ece56f020738c402f2a88e9dbe026571b286c2ab
Troy Butzin <tjbutzin@oakland.edu>
2018-10-06 18:03:37 -0400

    Site modifications (Landing page)
    Added Register button, working on CSS styling for the landing page.

--- frontend/client/src/components/landing/Main.js
+++ frontend/client/src/components/landing/Main.js
@@ -6,29 +6,29 @@ export default class LandingMain extends Component {
     return (
       <div>
         <Navbar />
-        <header class="masthead text-white text-center">
-          <div class="overlay" />
-          <div class="container">
-            <div class="row">
-              <div class="col-xl-9 mx-auto">
-                <h1 class="mb-5">
+        <header className="masthead text-white text-center">
+          <div className="overlay" />
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-9 mx-auto">
+                <h1 className="mb-5">
                   Your source for reliable weather information.
                 </h1>
               </div>
-              <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+              <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
                 <form>
-                  <div class="form-row">
-                    <div class="col-12 col-md-9 mb-2 mb-md-0">
+                  <div className="form-row">
+                    <div className="col-12 col-md-9 mb-2 mb-md-0">
                       <input
                         type="email"
-                        class="form-control form-control-lg"
+                        className="form-control form-control-lg"
                         placeholder="Enter your email..."
                       />
                     </div>
-                    <div class="col-12 col-md-3">
+                    <div className="col-12 col-md-3">
                       <button
                         type="submit"
-                        class="btn btn-block btn-lg btn-primary"
+                        className="btn btn-block btn-lg btn-primary"
                       >
                         Sign up!
                       </button>
@@ -39,39 +39,39 @@ export default class LandingMain extends Component {
             </div>
           </div>
         </header>
-        <section class="features-icons bg-light text-center">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-4">
-                <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                  <div class="features-icons-icon d-flex">
-                    <i class="icon-screen-desktop m-auto text-primary" />
+        <section className="features-icons bg-light text-center">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                  <div className="features-icons-icon d-flex">
+                    <i className="icon-screen-desktop m-auto text-primary" />
                   </div>
                   <h3>Fully Responsive</h3>
-                  <p class="lead mb-0">
+                  <p className="lead mb-0">
                     This theme will look great on any device, no matter the
                     size!
                   </p>
                 </div>
               </div>
-              <div class="col-lg-4">
-                <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                  <div class="features-icons-icon d-flex">
-                    <i class="icon-layers m-auto text-primary" />
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                  <div className="features-icons-icon d-flex">
+                    <i className="icon-layers m-auto text-primary" />
                   </div>
                   <h3>Bootstrap 4 Ready</h3>
-                  <p class="lead mb-0">
+                  <p className="lead mb-0">
                     Featuring the latest build of the new Bootstrap 4 framework!
                   </p>
                 </div>
               </div>
-              <div class="col-lg-4">
-                <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-                  <div class="features-icons-icon d-flex">
-                    <i class="icon-check m-auto text-primary" />
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-0 mb-lg-3">
+                  <div className="features-icons-icon d-flex">
+                    <i className="icon-check m-auto text-primary" />
                   </div>
                   <h3>Easy to Use</h3>
-                  <p class="lead mb-0">
+                  <p className="lead mb-0">
                     Ready to use with your own content, or customize the source
                     files!
                   </p>
--- frontend/client/src/components/landing/Navbar.js
+++ frontend/client/src/components/landing/Navbar.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react';
 export default class Navbar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-light bg-light static-top">
-        <div class="container">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-light bg-light static-top">
+        <div className="container">
+          <a className="navbar-brand" href="#">
             SkyPi
           </a>
-          <a class="btn btn-primary" href="#">
-            Sign In
-          </a>
+          <div className="button-container">
+            <button className="btn btn-primary" href="#">
+              Sign In
+            </button>
+            <button className="btn btn-primary" href="#">
+              Register
+            </button>
+          </div>
         </div>
       </nav>
     );
