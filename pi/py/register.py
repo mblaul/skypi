84b8fd67f1bafde1c541611bafe76a2bdc68a404
jfbauer04 <jfbauer@oakland.edu>
2018-10-07 18:25:58 -0400

    converted regisdter device to python

--- pi/py/register.py
+++ pi/py/register.py
@@ -1,13 +1,23 @@
 import requests
 import socket
+import json
 from uuid import getnode as get_mac
+url = "http://18.235.27.33/api/user/login" 
+payload = "email=jfbauer%40oakland.edu&password=432234" 
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}  
+response = requests.request("POST", url, data=payload, headers=headers)
+
+data = response.json()
+token = data['token']
+
 mac = str(get_mac())
+host = socket.gethostname()
 
 url = "http://18.235.27.33/api/device/register"
 
 payload = "name=" + host + "&macaddress="+ mac + "&model=Flyboi5000&ipaddress=192.168.1.100"
 headers = {
-            'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViOWIxMGEzNTk2OTc5MTEzMWE4MmI0NSIsIm5hbWUiOiJNYXJ0eSBNY0ZseSIsImlhdCI6MTUzNzIxODcxMywiZXhwIjoxNTM3MjIyMzEzfQ.wv70LlKmtdAU8A94NoC-mlDbhGhaMO3QYqBC0AhpveI",
+            'Authorization': token,
                 'Content-Type': "application/x-www-form-urlencoded"
                     }
 
