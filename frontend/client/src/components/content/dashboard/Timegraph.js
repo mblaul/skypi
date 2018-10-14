75ca17f1481d0ffadfde27908a0705deb12a6e64
Troy Butzin <tjbutzin@oakland.edu>
2018-10-14 11:34:49 -0400

    Chart now responsive

--- frontend/client/src/components/content/dashboard/Timegraph.js
+++ frontend/client/src/components/content/dashboard/Timegraph.js
@@ -8,8 +8,13 @@ const data = {
       data: [65, 59, 80, 81, 56, 55, 40],
       backgroundColor: ['rgba(105, 0, 132, .2)'],
       borderColor: ['rgba(200, 99, 132, .7)'],
-      borderWidth: 2
+      borderWidth: 2,
+      pointRadius: 10
       }]
+}
+
+const options = {
+  responsive: true
 }
 
 export default class Timegraph extends Component {
