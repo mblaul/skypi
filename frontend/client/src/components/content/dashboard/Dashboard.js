4937197c72a8453d531ab8fcaa5e894886485acb
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-10-16 11:09:56 -0400

    DashboardAndStationsChanges
    Made slight changes to Dashboard.js and Stations.js Added an AdminPage.js file with minor boilerplate code

--- frontend/client/src/components/content/dashboard/Dashboard.js
+++ frontend/client/src/components/content/dashboard/Dashboard.js
@@ -7,9 +7,28 @@ import Stripetable from '../dashboard/Stripetable';
 import Timegraph from '../dashboard/Timegraph';
 
 export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Name: "Daniel"
+      //The code below is to generate it based on a passed prop  
+      //Name: this.props.Name
+    };
+  }
   render() {
     return (
       <div className="container mt-2">
+            <header className="text-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6 mx-auto">
+            <div className="display-3 mb-3">
+              <h1>Hello {this.state.Name}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
         <div className="row mb-2">
           <div className="col-sm-12 col-md-12 col-lg-6">
             <Stripetable />
