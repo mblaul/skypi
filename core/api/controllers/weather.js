d88db411da1d6036d3956690b553a57545dfb7da
mblaul <32026169+mblaul@users.noreply.github.com>
2018-10-16 18:53:01 -0400

    Added routes to get public and private weather data

--- core/api/controllers/weather.js
+++ core/api/controllers/weather.js
@@ -63,6 +63,7 @@ module.exports.log_get = (req, res) => {
     });
 };
 
+<<<<<<< HEAD
 module.exports.data_get = (req, res) => {
   // Find logs for all weather data
   Weather.find({})
@@ -77,14 +78,19 @@ module.exports.data_get = (req, res) => {
       return res.status(500).json(errors);
     });
 };
+=======
+module.exports.data_mine_get = (req, res) => {
+  // Find all devices where you're an authorized user
+  Device.find({ "authorizedUsers.user": req.user.id })
+    .then(devices => {
+      // Collect all the device IDs from the authorized user field
+      const deviceIds = devices.map(device => {
+        return device._id;
+      });
+>>>>>>> Added routes to get weather data for public and private devices
 
-module.exports.onedevice_alllogs_get = (req, res) => {
-  // Find logs for all weather data
-  Device.find(device)
-    .then(device => {
-      Weather.find({ device }).then(logs => {
-        //Need to condition the data a little bit here
-        //So we're not returning so much information
+      // Find logs for the device IDs from above
+      Weather.find({ device: { $in: deviceIds } }).then(logs => {
         return res.json(logs);
       });
     })
@@ -95,13 +101,13 @@ module.exports.onedevice_alllogs_get = (req, res) => {
     });
 };
 
-module.exports.alldevices_lastlog_get = (req, res) => {
-  // Find logs for all weather data
-  Device.find(device)
-    .then(device => {
-      Weather.find({ device }, {}, { sort: { date: -1 } }).then(logs => {
-        return res.json(logs);
-      });
+module.exports.data_public_get = (req, res) => {
+  // Find all device logs for public devices
+
+  // Find logs for the device IDs from above
+  Weather.find({ "roles.isPublic": true })
+    .then(logs => {
+      return res.json(logs);
     })
     .catch(err => {
       console.log(err);
--- core/api/routes/weather.js
+++ core/api/routes/weather.js
@@ -1,31 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
-var weatherController = require('../controllers/weather');
+var weatherController = require("../controllers/weather");
 
 // @route   POST api/weather/log
 // @desc    Add weather log to the database
 // @access  Public
-router.post('/log', weatherController.log_post);
+router.post("/log", weatherController.log_post);
 
 // @route   GET api/weather/log
 // @desc    Get all weather logs for devices owned by the user
 // @access  Private
 router.get(
-  '/log',
-  passport.authenticate('jwt', { session: false }),
+  "/log",
+  passport.authenticate("jwt", { session: false }),
   weatherController.log_get
+);
+
+// @route   GET api/weather/data/mine
+// @desc    Get all weather data where you're an authorized user
+// @access  Private
+router.get(
+  "/data/mine",
+  passport.authenticate("jwt", { session: false }),
+  weatherController.data_mine_get
+);
+
+// @route   GET api/weather/data/public
+// @desc    Get all weather data that's public
+// @access  Private
+router.get(
+  "/data/public",
+  passport.authenticate("jwt", { session: false }),
+  weatherController.data_public_get
 );
 
 // @route   GET api/weather/data
 // @desc    Get all weather data
 // @access  Public
-router.get('/data', weatherController.data_get);
-
-// @route   GET api/weather/data
-// @desc    Get all weather data
-// @access  Public
-router.get('/lastlogs', weatherController.alldevices_lastlog_get);
+router.get("/lastlogs", weatherController.alldevices_lastlog_get);
 
 module.exports = router;
