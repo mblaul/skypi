78a323f82171f39c9baf07a9d78e333c75a01b3a
jfbauer04 <jfbauer@oakland.edu>
2018-10-18 10:56:07 -0400

    update file path

--- pi/py/mongo_api.py
+++ pi/py/mongo_api.py
@@ -2,7 +2,7 @@ import requests
 
 def pushdata(host,temperature,humidity,lat,lng,pressure,city, state, zip_code, country, wind,wind_direction):
 
-    with open('id.key', 'r') as myfile:
+    with open('/home/ansible/id.key', 'r') as myfile:
     device_key = myfile.read()
     
     url = "http://18.235.27.33/api/weather/log"
