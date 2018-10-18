e8c2af533f0cdbf2b0760e329ae11847d82cd777
Alex <woodle.alex@gmail.com>
2018-10-18 10:51:36 -0400

    Read in the device key to push to DB

--- pi/py/mongo_api.py
+++ pi/py/mongo_api.py
@@ -2,10 +2,13 @@ import requests
 
 def pushdata(host,temperature,humidity,lat,lng,pressure,city, state, zip_code, country, wind,wind_direction):
 
+    with open('id.key', 'r') as myfile:
+    device_key = myfile.read()
+    
     url = "http://18.235.27.33/api/weather/log"
 
-    payload = ("""source=%s&device=5b778422c1381d18a8bd003e&temperature=%f&humidity=%f&latitude=%f&longitude=%f&pressure=%f&city=%s&state=%s&zipcode=%f&country=%s&wind=%f&winddirection=%s"""
-    % (host, temperature, humidity, lat, lng, pressure, city, state, zip_code, country, wind, wind_direction))
+    payload = ("""source=%s&device=%s&temperature=%f&humidity=%f&latitude=%f&longitude=%f&pressure=%f&city=%s&state=%s&zipcode=%f&country=%s&wind=%f&winddirection=%s"""
+    % (host, device_key, temperature, humidity, lat, lng, pressure, city, state, zip_code, country, wind, wind_direction))
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
