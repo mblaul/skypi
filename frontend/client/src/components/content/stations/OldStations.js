1340703dfb2844c719503014a67fcfded4a8b2b7
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-10-17 20:55:41 -0400

    Changed Stations.js
    Created a OldStations.js for comtaining Legacy code, and Styled it to show both tables Side by side

--- frontend/client/src/components/content/stations/OldStations.js
+++ frontend/client/src/components/content/stations/OldStations.js
@@ -15,25 +15,20 @@ export default class Stations extends Component {
   }
   render() {
     return (
-      <div className="container mt-2">
+      <div className="wrapper">
         <header className="text-center">
           <div className="container">
             <div className="row">
               <div className="col-xl-6 mx-auto">
-                <div className="display-3 mb-3 my-2">
-                  <h1>Hello {`<get name from user state>`}</h1>
+                <div className="display-3 mb-3">
+                  <h1>Hello {this.state.Name}</h1>
                 </div>
               </div>
             </div>
           </div>
         </header>
-        <div className="row mb-2">
-          <div className="col-sm-12 col-md-12 col-lg-6">
-            <Stripetable />
-          </div>
-          <div className="col-sm-12 col-md-12 col-lg-6">
-            <Stripetable />
-          </div>
+        <div className="main-panel">
+          <Stripetable />
         </div>
       </div>
     );
