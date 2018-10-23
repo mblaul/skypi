ac18d84c62e5df9c332d0a6ab24a8c8e2a5e6431
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-10-23 04:43:45 -0400

    Dynamic Unit Conversions
    Added Dynamic Unit conversions based on an array of specified user preferences. Uses a hardcoded array, in furture will use data passed from the DB/API

--- frontend/client/src/components/content/dashboard/Stripetable.js
+++ frontend/client/src/components/content/dashboard/Stripetable.js
@@ -20,6 +20,38 @@ export default class Stripetable extends Component {
     //Function to change Admin Privledges in the Database
     window.alert(alertMessage)
   }
+  /*
+    Units for Temp, WindSpeed, and Pressure repectivly
+    Database uses °C , mps, and hPa
+    Temp Units °C and °F
+    WindSpd Units are KpH and MPH
+    Pressure Unitrs are KPa,  and PSI
+  */
+  tempConversions(DesiredUnit, tempToConvert) {
+    if (DesiredUnit === "Fahrenheit"){
+      return (((tempToConvert * 1.8) + 32).toFixed(2)) + " °F"
+    } else {
+      return tempToConvert + "°C";
+    }
+  }
+  speedConversions(DesiredUnit, speedToConvert) {
+    if (DesiredUnit === "MPH") {
+      return ((speedToConvert * 2.23694).toFixed(2)) + " MPH";
+    } else if (DesiredUnit === "KPH"){
+      return ((speedToConvert * 3.6).toFixed(2)) + " KPH";
+    } else {
+      return (speedToConvert) + " mps";
+    }
+  }
+  pressureConversions(DesiredUnit, pressureToConvert) {
+    if (DesiredUnit === "PSI") {
+      return ((pressureToConvert * 0.0145037738).toFixed(2)) + " PSI";
+    } else if (DesiredUnit === "KPa"){
+      return ((pressureToConvert * 0.1).toFixed(2)) + " KPa";
+    } else {
+      return (pressureToConvert) + " hPa"
+    }
+  }
   render() {
     // Declare an Array for the headers to display in the table
     const WeatherLogData = [
@@ -29,13 +61,15 @@ export default class Stripetable extends Component {
       this.props.weatherLogs[3],
       this.props.weatherLogs[4]
       ]
-      var DashboardSource = true
-      if (this.props.SourcePage === "Dashboard")
+    var DashboardSource = true
+    //Declare an Array of the users perfered units, in furute will be passed from the DB/API
+    const PreferedUnit = ["Celsius", "mps", "hPa"];
+    if (this.props.SourcePage === "Dashboard")
       {
         DashboardSource = true
-      } else {
+    } else {
         DashboardSource = false
-      }
+    }
     // Accept an Parameter to dictate when shows as the headers for Table Rows
     const tableHeaders = this.props.TableHeaders
       return (
@@ -57,7 +91,6 @@ export default class Stripetable extends Component {
                 ))}
               </tr>
             </thead>
-              {/* If this.props.SourcePage = Dashboard Render using the WeatherLogs Parameter */}
               {DashboardSource ?
                 /* If source Page is Dashboard.JS render Dashboard Stuff*/
                 (
@@ -69,20 +102,20 @@ export default class Stripetable extends Component {
                           {DataLog.date}
                         </Moment>
                       </td>
-                      <td> 
-                        {DataLog.temperature} °C
+                      <td>
+                          {this.tempConversions(PreferedUnit[0], DataLog.temperature)}
                       </td>
                       <td> 
                         {DataLog.humidity} %
                       </td>
                       <td> 
-                        {DataLog.wind} KPH
+                        {this.speedConversions(PreferedUnit[1], DataLog.wind)}
                       </td>
                       <td> 
                         {DataLog.winddirection}
                       </td>
                       <td>
-                        {DataLog.pressure}
+                        {this.pressureConversions(PreferedUnit[2], DataLog.pressure)}
                       </td>
                     </tr>
                   ))}
