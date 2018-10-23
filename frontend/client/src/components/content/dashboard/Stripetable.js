d593508f947d80ff597d0eef894543768eaf6c0e
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-10-23 00:41:27 -0400

    Various Changes
    Removed Comments and Previously Passed Parameters
    Added Functionality to display Pressure readings
    Added Logic to modify how tables are displayed based on the source page

--- frontend/client/src/components/content/dashboard/Stripetable.js
+++ frontend/client/src/components/content/dashboard/Stripetable.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import Moment from 'react-moment';
 
 export default class Stripetable extends Component {
+
+  resetUserPassword(UserID) {
+    var alertMessage = "Password Reset Email Sent To User"
+    //Delcare Dummy Function for resetting a User's Password
+    //Call Function to SEnd a Password Reset
+    //this.props.sendResetEmail(UserID);
+    window.alert(alertMessage);
+  }
+  deleteUserAccount(UserID) {
+    var alertMessage = "User Sucessfully Deleted"
+    //Function to delete user in the Database
+    window.alert(alertMessage)
+  }
+  changeAdminStatus(UserID, userName, AdminStatus) {
+    var alertMessage = "The User's Admin Status has been updated"
+    //Function to change Admin Privledges in the Database
+    window.alert(alertMessage)
+  }
   render() {
     // Declare an Array for the headers to display in the table
     const WeatherLogData = [
@@ -11,9 +29,14 @@ export default class Stripetable extends Component {
       this.props.weatherLogs[3],
       this.props.weatherLogs[4]
       ]
-    // Array for Dashboard Headers
-    //const tableHeaders = ['Date/Time', 'Temp', 'Humidity', 'Wind Speed', 'Wind Direction'];
-    // Attempt to Pass in an array to further dynamic capabilities
+      var DashboardSource = true
+      if (this.props.SourcePage === "Dashboard")
+      {
+        DashboardSource = true
+      } else {
+        DashboardSource = false
+      }
+    // Accept an Parameter to dictate when shows as the headers for Table Rows
     const tableHeaders = this.props.TableHeaders
       return (
       <div className="card">
@@ -34,32 +57,98 @@ export default class Stripetable extends Component {
                 ))}
               </tr>
             </thead>
-            <tbody>
-            {WeatherLogData.map(DataLog => (
-                <tr key={DataLog._id}>
-                  <td>                         
-                    <Moment format="YYYY/MM/DD h:mm A">
-                      {DataLog.date}
-                    </Moment>
-                  </td>
-                  <td> 
-                    {DataLog.temperature} °C
-                  </td>
-                  <td> 
-                    {DataLog.humidity} %
-                  </td>
-                  <td> 
-                    {DataLog.wind} KPH
-                  </td>
-                  <td> 
-                    {DataLog.winddirection}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+              {/* If this.props.SourcePage = Dashboard Render using the WeatherLogs Parameter */}
+              {DashboardSource ?
+                /* If source Page is Dashboard.JS render Dashboard Stuff*/
+                (
+                  <tbody>
+                  {WeatherLogData.map(DataLog => (
+                    <tr key={DataLog._id}>
+                      <td>                         
+                        <Moment format="YYYY/MM/DD h:mm A">
+                          {DataLog.date}
+                        </Moment>
+                      </td>
+                      <td> 
+                        {DataLog.temperature} °C
+                      </td>
+                      <td> 
+                        {DataLog.humidity} %
+                      </td>
+                      <td> 
+                        {DataLog.wind} KPH
+                      </td>
+                      <td> 
+                        {DataLog.winddirection}
+                      </td>
+                      <td>
+                        {DataLog.pressure}
+                      </td>
+                    </tr>
+                  ))}
+                  </tbody>
+                ):
+                /* If source Page is AdminPage.JS render AdminPage Stuff*/
+                (
+                <tbody>
+                  {WeatherLogData.map(DataLog => (
+                    <tr key={DataLog._id}>
+                      <td>                         
+                        {DataLog.userName}
+                      </td>
+                      <td> 
+                        {DataLog.email}
+                      </td>
+                      <td> 
+                        <button 
+                          onClick={this.resetUserPassword.bind("Reset Email Sent")} 
+                          type="button" 
+                          className="btn" >
+                          Reset Password
+                        </button>
+                      </td>
+                      <td> 
+                        {DataLog.Admin ?
+                        (
+                          <React.Fragment>
+                            <input 
+                                type='checkbox' 
+                                name={DataLog._id} 
+                                onChange={this.changeAdminStatus.bind(DataLog._id, DataLog.Admin)}
+                                defaultChecked >
+                              </input>
+                          </React.Fragment>
+                        ): (
+                          <React.Fragment>
+                            <input 
+                                type='checkbox' 
+                                name={DataLog._id} 
+                                onChange={this.changeAdminStatus.bind(DataLog._id, DataLog.name, DataLog.Admin)} >
+                              </input>
+                          </React.Fragment>
+                        )}
+                      </td>
+                      <td> 
+                        <button 
+                          onClick={this.deleteUserAccount.bind("")}
+                          type="button" 
+                          className="btn" > 
+                          Delete Account 
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                  </tbody>
+                )}
+            
           </table>
         </div>
       </div>
     );
+//End Render Bracket
   }
+  DummyFunction() {
+
+  }
+//End Class Bracket
 }
