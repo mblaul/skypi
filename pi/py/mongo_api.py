bd7a6a12149fd69c0944ec9260d0114005beade8
Alex <woodle.alex@gmail.com>
2018-10-23 21:27:09 -0400

    updated data with percipitation/description

--- pi/py/mongo_api.py
+++ pi/py/mongo_api.py
@@ -1,14 +1,14 @@
 import requests
 
-def pushdata(host,temperature,humidity,lat,lng,pressure,city, state, zip_code, country, wind,wind_direction):
+def pushdata(host,temperature,humidity,lat,lng,pressure,city, state, zip_code, country, wind,wind_direction,percipitation,description):
 
     with open('/home/ansible/id.key', 'r') as myfile:
         device_key = myfile.read()
     
     url = "http://18.235.27.33/api/weather/log"
 
-    payload = ("""source=%s&device=%s&temperature=%s&humidity=%s&latitude=%f&longitude=%f&pressure=%s&city=%s&state=%s&zipcode=%s&country=%s&wind=%f&winddirection=%s"""
-    % (host, device_key, temperature, humidity, lat, lng, pressure, city, state, zip_code, country, wind, wind_direction))
+    payload = ("""source=%s&device=%s&temperature=%s&humidity=%s&latitude=%f&longitude=%f&pressure=%s&city=%s&state=%s&zipcode=%s&country=%s&wind=%f&winddirection=%s&precipitation=%s&description=%s"""
+    % (host, device_key, temperature, humidity, lat, lng, pressure, city, state, zip_code, country, wind, wind_direction,percipitation,description))
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
