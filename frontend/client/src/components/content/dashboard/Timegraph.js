11913959a639b3ce49324f719f378ef93d6b145d
Troy Butzin <tjbutzin@oakland.edu>
2018-10-28 18:36:29 -0400

    Added additional points, axis min/max, and chart headings to Timegraph.js

--- frontend/client/src/components/content/dashboard/Timegraph.js
+++ frontend/client/src/components/content/dashboard/Timegraph.js
@@ -6,6 +6,9 @@ export default class Timegraph extends Component {
     const moment = require('moment');
     const data = {
       labels: [
+        moment(this.props.weatherDates[7]).format("h:mm A"),
+        moment(this.props.weatherDates[6]).format("h:mm A"),
+        moment(this.props.weatherDates[5]).format("h:mm A"),
         moment(this.props.weatherDates[4]).format("h:mm A"), 
         moment(this.props.weatherDates[3]).format("h:mm A"), 
         moment(this.props.weatherDates[2]).format("h:mm A"), 
@@ -16,6 +19,9 @@ export default class Timegraph extends Component {
         {
           label: this.props.chartLabel,
           data: [
+            this.props.weatherLogs[7],
+            this.props.weatherLogs[6],
+            this.props.weatherLogs[5],
             this.props.weatherLogs[4],
             this.props.weatherLogs[3],
             this.props.weatherLogs[2],
@@ -32,6 +38,19 @@ export default class Timegraph extends Component {
     };
     const options = {
       responsive: true,
+      scales: {
+        yAxes: [{
+            ticks: {
+                suggestedMin: 25,
+                suggestedMax: 75
+            }
+        }]
+      },
+      title: {
+        display: true,
+        text: this.props.chartLabel,
+        fontSize: 18
+      }
     };
 
     return (
