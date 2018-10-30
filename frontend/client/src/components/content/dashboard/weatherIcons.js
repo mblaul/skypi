515b8b09625fe742cf19914c8ac01546a44c1ef4
Troy Butzin <tjbutzin@oakland.edu>
2018-10-30 11:33:51 -0400

    weatherIcons function fully operational

--- frontend/client/src/components/content/dashboard/weatherIcons.js
+++ frontend/client/src/components/content/dashboard/weatherIcons.js
@@ -1,14 +1,24 @@
-const weatherDesc = weatherLogs.map((logs) => logs.description);
-
 export default function weatherIcons(weatherDesc){
     switch(weatherDesc){
-        case "Cloudy": 
-            return console.log("Cloudyicon");
-            break;
-        case "Sunny":
-            return console.log("Sunnyicon");
-            break;
+        case "partly-cloudy-day":
+            return '/img/climacons/Cloud-Sun.svg';
+        case "partly-cloudy-night":
+            return '/img/climacons/Cloud-Moon.svg';
+        case "clear-night":
+            return '/img/climacons/Moon.svg';
+        case "cloudy":
+            return '/img/climacons/Cloud.svg';
+        case "rain":
+            return '/img/climacons/Cloud-Drizzle.svg';
+        case "sleet":
+            return '/img/climacons/Cloud-Hail-Alt.svg';
+        case "snow":
+            return '/img/climacons/Cloud-Snow-Alt.svg';
+        case "wind":
+            return '/img/climacons/Cloud-Wind.svg';
+        case "fog":
+            return '/img/climacons/Cloud-Fog.svg';
         default:
-            return undefined;
+            return '/img/climacons/Sun.svg';
     }
 }
