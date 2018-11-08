84a44128b64252fb611e436a41f0732d2ff72fc5
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-11-08 18:40:30 -0500

    Reconfigured for better flow
    Reconfigured the page to allow it to accept passed parameters, return values (as a string) and use only one function to make all conversions for fewer points of potential failure

--- frontend/client/src/components/content/dashboard/UnitConversions.js
+++ frontend/client/src/components/content/dashboard/UnitConversions.js
@@ -1,24 +1,46 @@
-import React, { Component } from 'react';
-
-class UnitConversions extends Component{
-    tempConversions(tempToConvert, convertUnit) {
-        if (convertUnit)
-        {
-            return (tempToConvert * 1.8 + 32).toFixed(2) + ' °F';
-        }
+export default function UnitConversions(unitToConvert, convertUnit, reading){
+  if (convertUnit){
+    if (reading == 'Temp') 
+    {
+      return (unitToConvert * 1.8 + 32).toFixed(2) + ' °F';
+    } else if (reading == 'wind') {
+      return (unitToConvert * 2.23694).toFixed(2) + ' MPH';
+    } else if (reading == 'Pressure') {
+      return (unitToConvert * 0.0145037738).toFixed(2) + ' PSI';
+    } else {
+      if (reading == 'Wind') 
+      {
+        return (unitToConvert * 3.6).toFixed(2) + 'KPH';
+      } else if (reading == 'Presure') {
+        return (unitToConvert + 'mbar');
       }
-      speedConversions(speedToConvert, convertUnit) {
-        if (convertUnit) {
-          return (speedToConvert * 2.23694).toFixed(2) + ' MPH';
-        } else {
-          return (speedToConvert * 3.6).toFixed(2) + 'KPH';
-        }
-      }
-      pressureConversions(pressureToConvert, convertUnit) {
-        if (convertUnit) {
-          return (pressureToConvert * 0.0145037738).toFixed(2) + ' PSI';
-        } else {
-          return (pressureToConvert + 'mbar');
-        }
     }
+  } 
 }
+    /*
+     ----- Previous Conversion Formulas and logic-----
+    *  import React, { Component } from 'react';
+    *
+    *  class UnitConversions extends Component{
+    *      tempConversions(tempToConvert, convertUnit) {
+    *          if (convertUnit)
+    *          {
+    *              return (tempToConvert * 1.8 + 32).toFixed(2) + ' °F';
+    *          }
+    *        }
+    *        speedConversions(speedToConvert, convertUnit) {
+    *          if (convertUnit) {
+    *            return (speedToConvert * 2.23694).toFixed(2) + ' MPH';
+    *          } else {
+    *            return (speedToConvert * 3.6).toFixed(2) + 'KPH';
+    *          }
+    *        }
+    *        pressureConversions(pressureToConvert, convertUnit) {
+    *          if (convertUnit) {
+    *            return (pressureToConvert * 0.0145037738).toFixed(2) + ' PSI';
+    *          } else {
+    *            return (pressureToConvert + 'mbar');
+    *          }
+    *      }
+    *  }
+    */
