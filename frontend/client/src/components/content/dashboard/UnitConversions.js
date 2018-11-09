18894b94c5689d56a9ae5cd585f42ae9d5c2fb46
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-11-08 19:43:29 -0500

    Minor Text changes

--- frontend/client/src/components/content/dashboard/UnitConversions.js
+++ frontend/client/src/components/content/dashboard/UnitConversions.js
@@ -3,7 +3,7 @@ export default function UnitConversions(unitToConvert, convertUnit, reading){
     if (reading === 'Temp') 
     {
       return (unitToConvert * 1.8 + 32).toFixed(2) + ' °F';
-    } else if (reading === 'wind') {
+    } else if (reading === 'Wind') {
       return (unitToConvert * 2.23694).toFixed(2) + ' MPH';
     } else if (reading === 'Pressure') {
       return (unitToConvert * 0.0145037738).toFixed(2) + ' PSI';
