954cee7a3b13fb1085908c9ab17488cfb1e69d8f
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-11-08 21:39:44 -0500

    UnitConversions
    Modified the Data array initalizations. instead of passing the data directly it converts the data (or skips if the User Prefers Metric), parses the result into a float (floats convert easier than doubles) and then writes that into the Data array

--- frontend/client/src/components/content/dashboard/Timegraph.js
+++ frontend/client/src/components/content/dashboard/Timegraph.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
+import UnitConversions from './UnitConversions';
 
 export default class Timegraph extends Component {
   render() {
     const moment = require('moment');
+    //Temp boolean called 'convertUnits' to control if units are converted
+    const convertUnits = true;
+    // true => Convert Readings to the Imperial system
+    // flase => Don't convert (keep readings in the Metric system)
+    //Declare a String variable for what readingType to convert, initalized to whatever the Chart's label is
+    const readingType = this.props.chartLabel;
     const data = {
       labels: 
         (this.props.weatherDates.map((weatherDates, i) => {
@@ -24,15 +31,28 @@ export default class Timegraph extends Component {
         {
           label: this.props.chartLabel,
           data: [
-            this.props.weatherLogs[7],
-            this.props.weatherLogs[6],
-            this.props.weatherLogs[5],
-            this.props.weatherLogs[4],
-            this.props.weatherLogs[3],
-            this.props.weatherLogs[2],
-            this.props.weatherLogs[1],
-            this.props.weatherLogs[0]
+            parseFloat(UnitConversions(this.props.weatherLogs[7], convertUnits, readingType)),
+            parseFloat(UnitConversions(this.props.weatherLogs[6], convertUnits, readingType)),
+            parseFloat(UnitConversions(this.props.weatherLogs[5], convertUnits, readingType)),
+            parseFloat(UnitConversions(this.props.weatherLogs[4], convertUnits, readingType)),
+            parseFloat(UnitConversions(this.props.weatherLogs[3], convertUnits, readingType)),
+            parseFloat(UnitConversions(this.props.weatherLogs[2], convertUnits, readingType)),
+            parseFloat(UnitConversions(this.props.weatherLogs[1], convertUnits, readingType)),
+            parseFloat(UnitConversions(this.props.weatherLogs[0], convertUnits, readingType)),
           ],
+          /*
+          *-----Previous Data Initalization-----
+          *data: [
+          *  this.props.weatherLogs[7],
+          *  this.props.weatherLogs[6],
+          *  this.props.weatherLogs[5],
+          *  this.props.weatherLogs[4],
+          *  this.props.weatherLogs[3],
+          *  this.props.weatherLogs[2],
+          *  this.props.weatherLogs[1],
+          *  this.props.weatherLogs[0]
+          *],
+          */
           backgroundColor: ['rgba(0, 0, 0, 0)'],
           borderColor: ['rgba(0, 102, 255, .7)'],
           borderWidth: 2,
