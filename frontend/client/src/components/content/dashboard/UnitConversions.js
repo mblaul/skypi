5ccaa9ee2f64d670dd0789e518edede756bf2f05
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-11-08 21:38:14 -0500

    Added Logic
    Added Logic so no conversions happen on Humidity. Better for generically calling it from other functions

--- frontend/client/src/components/content/dashboard/UnitConversions.js
+++ frontend/client/src/components/content/dashboard/UnitConversions.js
@@ -2,11 +2,13 @@ export default function UnitConversions(unitToConvert, convertUnit, reading){
   if (convertUnit){
     if (reading === 'Temperature') 
     {
-      return (unitToConvert * 1.8 + 32).toFixed(2) + ' °F';
+      return (unitToConvert * 1.8 + 32).toFixed(2) + ' °F ';
     } else if (reading === 'Wind') {
       return (unitToConvert * 2.23694).toFixed(2) + ' MPH';
     } else if (reading === 'Pressure') {
       return (unitToConvert * 0.0145037738).toFixed(2) + ' PSI';
+    } else if (reading === 'Humidity'){
+      return unitToConvert;
     } else {
       if (reading === 'Wind') 
       {
