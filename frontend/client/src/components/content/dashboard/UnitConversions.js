b0ff68fc243809ab390d9ba9f9ac28b36746d86e
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-11-08 22:49:38 -0500

    Restructured Logic
    Restructured the conditional logic, function now properly handles if the user prefers Metric units and displays results accordingly

--- frontend/client/src/components/content/dashboard/UnitConversions.js
+++ frontend/client/src/components/content/dashboard/UnitConversions.js
@@ -1,5 +1,6 @@
 export default function UnitConversions(unitToConvert, convertUnit, reading){
-  if (convertUnit){
+  if (convertUnit)
+  {
     if (reading === 'Temperature') 
     {
       return (unitToConvert * 1.8 + 32).toFixed(2) + ' °F ';
@@ -9,15 +10,19 @@ export default function UnitConversions(unitToConvert, convertUnit, reading){
       return (unitToConvert * 0.0145037738).toFixed(2) + ' PSI';
     } else if (reading === 'Humidity'){
       return unitToConvert;
-    } else {
-      if (reading === 'Wind') 
-      {
-        return (unitToConvert * 3.6).toFixed(2) + 'KPH';
-      } else if (reading === 'Presure') {
-        return (unitToConvert + 'mbar');
-      }
-    }
-  } 
+    } 
+  } else {
+    if (reading === 'Temperature') 
+    {
+      return unitToConvert + ' °C ';
+    } else if (reading === 'Wind') {
+      return (unitToConvert * 3.6).toFixed(2) + 'KPH';
+    } else if (reading === 'Pressure') {
+      return (unitToConvert * 0.1).toFixed(2) + ' kpa';
+    } else if (reading === 'Humidity'){
+      return unitToConvert;
+    }   
+  }
 }
     /*
      ----- Previous Conversion Formulas and logic-----
