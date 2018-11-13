33cd0d9192940c63f7c3fb04d70eed73b5858821
Troy Butzin <tjbutzin@oakland.edu>
2018-11-13 11:12:47 -0500

    Revert "Modifications to Timegraph to take all device data"
    This reverts commit f8f9ab7d43c9b9830ff37f71ff4bb6f2eb141327.

--- frontend/client/src/components/content/dashboard/Timegraph.js
+++ frontend/client/src/components/content/dashboard/Timegraph.js
@@ -11,36 +11,87 @@ export default class Timegraph extends Component {
     // false => Don't convert (keep readings in the Metric system)
     //Declare a String variable for what readingType to convert, initalized to whatever the Chart's label is
     const readingType = this.props.chartLabel;
-    let visibleData = [];
     const data = {
       labels: 
       this.props.weatherDates.map((weatherDates, i) => 
       {
-        return moment(this.props.weatherDates[i]).format("YYYY/MM/DD h:mm A");
+        return moment(this.props.weatherDates[i]).format("h:mm A");
       })
       .reverse(),
       datasets: [
         {
           label: this.props.chartLabel,
           data: [
-            this.props.weatherLogs.map((weatherLogs, i) => 
-            {
-              visibleData = parseFloat(
-                UnitConversions(
-                  this.props.weatherLogs[i], convertUnits, readingType
-                )
+            parseFloat(
+              UnitConversions(
+                this.props.weatherLogs[7],
+                convertUnits,
+                readingType
               )
-              return visibleData;
-            })
-            .reverse()
-            /* parseFloat(
+            ),
+            parseFloat(
+              UnitConversions(
+                this.props.weatherLogs[6],
+                convertUnits,
+                readingType
+              )
+            ),
+            parseFloat(
+              UnitConversions(
+                this.props.weatherLogs[5],
+                convertUnits,
+                readingType
+              )
+            ),
+            parseFloat(
+              UnitConversions(
+                this.props.weatherLogs[4],
+                convertUnits,
+                readingType
+              )
+            ),
+            parseFloat(
+              UnitConversions(
+                this.props.weatherLogs[3],
+                convertUnits,
+                readingType
+              )
+            ),
+            parseFloat(
+              UnitConversions(
+                this.props.weatherLogs[2],
+                convertUnits,
+                readingType
+              )
+            ),
+            parseFloat(
+              UnitConversions(
+                this.props.weatherLogs[1],
+                convertUnits,
+                readingType
+              )
+            ),
+            parseFloat(
               UnitConversions(
                 this.props.weatherLogs[0],
                 convertUnits,
                 readingType
               )
-            ) */
+            )
           ],
+          /*
+           *-----Previous Data Initalization-----
+           *data: [
+           *  this.props.weatherLogs[7],
+           *  this.props.weatherLogs[6],
+           *  this.props.weatherLogs[5],
+           *  this.props.weatherLogs[4],
+           *  this.props.weatherLogs[3],
+           *  this.props.weatherLogs[2],
+           *  this.props.weatherLogs[1],
+           *  this.props.weatherLogs[0]
+           *],
+           */
           backgroundColor: ['rgba(0, 0, 0, 0)'],
           borderColor: ['rgba(0, 102, 255, .7)'],
           borderWidth: 2,
