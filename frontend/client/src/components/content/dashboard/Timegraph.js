ba26352e99d08ad14cfade1b1c8cca18719e79b1
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-11-15 00:42:06 -0500

    Conversions and Dynamic Creation
    Configured TimeGraph to Convert Units
    Configured Timegraph to display a more appropriate scale
    Changed from a map function to a for loop, giving us control of the number of readings displayed
    Added a boolean prop to control if readings displayed is limited

--- frontend/client/src/components/content/dashboard/Timegraph.js
+++ frontend/client/src/components/content/dashboard/Timegraph.js
@@ -6,11 +6,18 @@ export default class Timegraph extends Component {
   render() {
     const moment = require('moment');
     //Temp boolean called 'convertUnits' to control if units are converted
-    const convertUnits = false;
+    const convertUnits = true;
     // true => Convert Readings to the Imperial system
     // false => Don't convert (keep readings in the Metric system)
     //Declare a String variable for what readingType to convert, initalized to whatever the Chart's label is
     const readingType = this.props.chartLabel;
+    //Declare variable to limit how many records are displayed
+    var readingsToDisplay = this.props.weatherLogs.length;
+    const switcher = this.props.limitDisplay;
+    if (switcher === true){
+      readingsToDisplay = 10;
+    }
+    //const readingsToDisplay = 100;
     // this.props.weatherLogs.map((weatherLogs, i) => 
     // {
     //   console.log(this.props.weatherLogs[i]);
@@ -23,18 +30,32 @@ export default class Timegraph extends Component {
     //Creates a new variable and brings the weatherLogs data into it, ideally the data would be modified
     //to whatever range the user wants in Dashboard and then passed into Timegraph
     let weather2Logs = [];
-    weather2Logs = this.props.weatherLogs.map((weatherLogs, i) => 
-            {
-              console.log(this.props.weatherLogs[i]);
-              return this.props.weatherLogs[i];
-            });
+    for (let j = 0; j < readingsToDisplay; j++) 
+    {
+      weather2Logs[j] = parseFloat(UnitConversions(this.props.weatherLogs[j], convertUnits, readingType));
+    }
+    let weather2Dates = [];     
+    for (let j = 0; j < readingsToDisplay; j++) 
+    {
+      weather2Dates[j] = moment(this.props.weatherDates[j]).format("MM-DD h:mm A");
+    }
+    // Creating A for loop to allow for control of how many values are displayed
+    // weather2Logs = this.props.weatherLogs.map((weatherLogs, i) => 
+    //   {
+    //     //console.log(this.props.weatherLogs[i]);
+    //     //NewReturn Statement does convert data
+    //     return parseFloat(UnitConversions(this.props.weatherLogs[i], convertUnits, readingType))
+    //     //Old Return statement doesn't convert data
+    //     //return this.props.weatherLogs[i];
+    //   });
     const data = {
-      labels: 
-      this.props.weatherDates.map((weatherDates, i) => 
-      {
-        console.log(this.props.weatherDates[i]);
-        return moment(this.props.weatherDates[i]).format("h:mm A");
-      })
+      labels: weather2Dates
+      // Converting to a for loop to limit how many records are displayed
+      // this.props.weatherDates.map((weatherDates, i) => 
+      // {
+      //   //console.log(this.props.weatherDates[i]);
+      //   return moment(this.props.weatherDates[i]).format("h:mm A");
+      // })
       .reverse(),
       datasets: [
         {
@@ -56,8 +77,12 @@ export default class Timegraph extends Component {
         yAxes: [
           {
             ticks: {
-              suggestedMin: 5,
-              suggestedMax: 50
+              //Trying to Dynamically change ticks
+              suggestedMin: ((Math.min.apply(null, weather2Logs) - 5)),
+              suggestedMax: ((Math.max.apply(null, weather2Logs) + 5))
+              //Old way of declaring ticks
+              //suggestedMin: 5,
+              //suggestedMax: 50
             }
           }
         ]
