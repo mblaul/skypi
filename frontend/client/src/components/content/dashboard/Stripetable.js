0a509be9d6fca537cd334ee2f8aa2b8ea7e5eff5
DBWADE <33231832+DBWADE6633@users.noreply.github.com>
2018-11-15 00:40:26 -0500

    TrimmedCode
    Removed unneeded conversions functions (moved to it's own file

--- frontend/client/src/components/content/dashboard/Stripetable.js
+++ frontend/client/src/components/content/dashboard/Stripetable.js
@@ -23,40 +23,6 @@ class Stripetable extends Component {
       alert(`${userEmail} has been spared.`);
     }
   }
-  /*
-    Units for Temp, WindSpeed, and Pressure repectivly
-    Database uses °C , mps, and hPa
-    Temp Units °C and °F
-    WindSpd Units are KpH and MPH
-    Pressure Unitrs are KPa,  and PSI
-  */
-  tempConversions(DesiredUnit, tempToConvert) {
-    if (DesiredUnit === 'Fahrenheit') {
-      return (tempToConvert * 1.8 + 32).toFixed(2) + ' °F';
-    } else {
-      return tempToConvert + '°C';
-    }
-  }
-  speedConversions(DesiredUnit, speedToConvert) {
-    if (DesiredUnit === 'MPH') {
-      return (speedToConvert * 2.23694).toFixed(2) + ' MPH';
-    } else if (DesiredUnit === 'KPH') {
-      return (speedToConvert * 3.6).toFixed(2) + ' KPH';
-    } else {
-      return speedToConvert + ' mps';
-    }
-  }
-  pressureConversions(DesiredUnit, pressureToConvert) {
-    if (DesiredUnit === 'PSI') {
-      return (pressureToConvert * 0.0145037738).toFixed(2) + ' PSI';
-    } else if (DesiredUnit === 'KPa') {
-      return (pressureToConvert * 0.1).toFixed(2) + ' KPa';
-    } else if (DesiredUnit === 'mb'){
-      return (pressureToConvert + 'mbar');
-    }  {
-      return pressureToConvert + ' hPa';
-    }
-  }
   render() {
     // Accept an Parameter to dictate when shows as the headers for Table Rows
     const { tableHeaders, data } = this.props;
