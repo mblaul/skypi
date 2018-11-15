a1e63d8df866b57a470fa64b3f66264a0b0ffe49
Daniel <dfciesie@oakland.edu>
2018-11-15 12:44:25 -0500

    marked each device

--- frontend/client/src/components/common/Map.js
+++ frontend/client/src/components/common/Map.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getPublicDevices } from '../../actions/deviceActions';
+
 import GoogleMapReact from 'google-map-react';
 import MapMarker from './MapMarker';
 
 const API_KEY = "AIzaSyDF7OuqlgIjCV8SFZ5BVH23pGK4-UQ1qlY";
  
 class Map extends Component {
+  componentDidMount() {
+    this.props.getPublicDevices();
+  }
+
   static defaultProps = {
     center: {
       lat: 42.6679,
@@ -14,6 +22,8 @@ class Map extends Component {
   };
  
   render() {
+    const { devices } = this.props.devices;
+    console.log(devices);
     return (
       //The map will fill the size of the container
       <div style={{ height: '90vh', width: '100%' }}>
@@ -22,15 +32,34 @@ class Map extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <MapMarker
+          {devices.map((device) =>
+            <MapMarker
+              lat={device.lastWeatherLog.latitude}
+              lng={device.lastWeatherLog.longitude}
+              text={device.name}
+            />
+          )}
+          {/* <MapMarker
             lat={42.66}
             lng={-83.20}
             text={'A'}
-          />
+          /> */}
         </GoogleMapReact>
       </div>
     );
   }
 }
- 
-export default Map;
+
+Map.propTypes = {
+  getPublicDevices: PropTypes.func.isRequired,
+  devices: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  devices: state.devices
+})
+
+export default connect(
+  mapStateToProps,
+  { getPublicDevices }
+)(Map);
