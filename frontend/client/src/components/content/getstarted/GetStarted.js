395c43392c05ca6ff78667ad8fe94a58880edd8d
mblaul <mattblaul@gmail.com>
2018-11-27 21:00:13 -0500

    Prettied up the get started page a bit

--- frontend/client/src/components/content/getstarted/GetStarted.js
+++ frontend/client/src/components/content/getstarted/GetStarted.js
@@ -7,7 +7,6 @@ import { getFavoriteWeatherData } from '../../../actions/weatherActions';
 import { getPublicDevices } from '../../../actions/deviceActions';
 import { getUserPreferences } from '../../../actions/authActions';
 
-
 class GetStarted extends Component {
   constructor() {
     super();
@@ -17,7 +16,7 @@ class GetStarted extends Component {
       endDate: ''
     };
   }
-  
+
   componentDidMount() {
     if (!this.props.auth) {
       this.props.history.push('/login');
@@ -36,52 +35,139 @@ class GetStarted extends Component {
   render() {
     return (
       <div className="container mt-2">
-        <div className="row mb-3">
-          <div className="display-3 my-3">
-            Hello, {this.props.auth.user.name}
+        <div className="col-sm-12 col-md-12 col-lg-12">
+          <div className="my-4">
+            <div className="display-4 my-3">
+              So you want to bake a{' '}
+              <span role="img" aria-label="pi">
+                🥧
+              </span>
+              ?
+            </div>
+            <lead className="muted">
+              Listed below are instructions for adding your own Pi to our
+              system.
+            </lead>
           </div>
-          <hr />
-        </div>
-        <div className="row">
-        <div className="col-sm-12 col-md-12 col-lg-8">
-        <p>Listed below are instructions for adding your own Pi to our system.</p>
-        <h4>New Device with SkyPi Image</h4>
-            <ol>
-            <li>Download Image and install on micro SD</li>
-            <li>On your Mobile Device, install Berry Lan and Termius</li>
-            <li>Open Berry Lan and select your device (skypi)</li> 
-            <li>Select your wifi Network, and enter the credentials</li>
-            <li>Make note of the displayed IP address</li> 
-            <li>Connect to your device in Termius using the username skypi and password weather</li>
-            <li>Run the following commands:</li>
-                <ol>
-                <li><pre>cd skypi</pre></li>
-                <li><pre>sudo python3 new_station.py</pre></li>
-                </ol>
-            <li>Follow the instructions to finish setup</li>
-            </ol>
-            <p></p>
-        <h4>Existing Device</h4>
-            <ol>
-            <li>Ensure that you have Python 3 and Pip for Python 3 installed</li>
-            <li>Ensure that Git is installed</li>
-                <ol>
-                <li><pre>sudo apt install git</pre></li>
-                </ol>
-            <li>Clone the GitHub repository below:</li>
-                <ol>
-                <li><pre>git clone https://github.com/alexanderwoodle/skypi</pre></li>
-                </ol>
-            <li>Enter the following command to change into the SkyPi directory to install the required modules, and register your device</li>
-                <ol>
-                <li><pre>cd skypi</pre></li>
-                <li><pre>sudo pip3 install -r requirements.txt</pre></li>
-                <li><pre>sudo python3 new_station.py</pre></li>
-                </ol>
-            <li>Follow the instructions to finish setup</li>
-            </ol>
-        <h4>Click <a href="https://drive.google.com/a/oakland.edu/file/d/1q-83aGAs-a4eRIyc31KtDajOA2Egix8C/view?usp=sharing">HERE</a> to download the SkyPi Raspbian Image</h4>  
-        </div>
+          <div id="accordion">
+            <div class="card">
+              <div class="card-header" id="headingOne">
+                <h5 class="mb-0">
+                  <button
+                    class="btn btn-link"
+                    data-toggle="collapse"
+                    data-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    <h5>New Device with SkyPi Image</h5>
+                  </button>
+                </h5>
+              </div>
+
+              <div
+                id="collapseOne"
+                class="collapse show"
+                aria-labelledby="headingOne"
+                data-parent="#accordion"
+              >
+                <div class="card-body">
+                  <ol>
+                    <li>Download Image and install on micro SD</li>
+                    <li>
+                      On your Mobile Device, install Berry Lan and Termius
+                    </li>
+                    <li>Open Berry Lan and select your device (skypi)</li>
+                    <li>Select your wifi Network, and enter the credentials</li>
+                    <li>Make note of the displayed IP address</li>
+                    <li>
+                      Connect to your device in Termius using the username skypi
+                      and password weather
+                    </li>
+                    <li>Run the following commands:</li>
+                    <ol>
+                      <li>
+                        <pre>cd skypi</pre>
+                      </li>
+                      <li>
+                        <pre>sudo python3 new_station.py</pre>
+                      </li>
+                    </ol>
+                    <li>Follow the instructions to finish setup</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingTwo">
+                <h5 class="mb-0">
+                  <button
+                    class="btn btn-link collapsed"
+                    data-toggle="collapse"
+                    data-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                  >
+                    <h5>Existing Device</h5>
+                  </button>
+                </h5>
+              </div>
+              <div
+                id="collapseTwo"
+                class="collapse"
+                aria-labelledby="headingTwo"
+                data-parent="#accordion"
+              >
+                <div class="card-body">
+                  <ol>
+                    <li>
+                      Ensure that you have Python 3 and Pip for Python 3
+                      installed
+                    </li>
+                    <li>Ensure that Git is installed</li>
+                    <ol>
+                      <li>
+                        <pre>sudo apt install git</pre>
+                      </li>
+                    </ol>
+                    <li>Clone the GitHub repository below:</li>
+                    <ol>
+                      <li>
+                        <pre>
+                          git clone https://github.com/alexanderwoodle/skypi
+                        </pre>
+                      </li>
+                    </ol>
+                    <li>
+                      Enter the following command to change into the SkyPi
+                      directory to install the required modules, and register
+                      your device
+                    </li>
+                    <ol>
+                      <li>
+                        <pre>cd skypi</pre>
+                      </li>
+                      <li>
+                        <pre>sudo pip3 install -r requirements.txt</pre>
+                      </li>
+                      <li>
+                        <pre>sudo python3 new_station.py</pre>
+                      </li>
+                    </ol>
+                    <li>Follow the instructions to finish setup</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <h4 className="mt-4">
+            Click{' '}
+            <a href="https://drive.google.com/a/oakland.edu/file/d/1q-83aGAs-a4eRIyc31KtDajOA2Egix8C/view?usp=sharing">
+              HERE
+            </a>{' '}
+            to download the SkyPi Raspbian Image
+          </h4>
         </div>
       </div>
     );
