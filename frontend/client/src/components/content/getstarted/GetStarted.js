17d6dafb26e06e64c8fc74d441b2c4b6a89c0e76
Alex <woodle.alex@gmail.com>
2018-11-29 15:29:27 -0500

    added more instructions

--- frontend/client/src/components/content/getstarted/GetStarted.js
+++ frontend/client/src/components/content/getstarted/GetStarted.js
@@ -77,6 +77,16 @@ class GetStarted extends Component {
                     <li>
                       On your Mobile Device, install Berry Lan and Termius
                     </li>
+                    <ul>
+                      <li>Click{' '} <a href="https://berrylan.app/">
+                          here</a>{' '}
+                          to visit the BerryLan website
+                      </li>
+                      <li>Click{' '} <a href="https://www.termius.com/">
+                          here</a>{' '}
+                          to visit the Termius website
+                      </li>
+                    </ul>
                     <li>Open Berry Lan and select your device (skypi)</li>
                     <li>Select your wifi Network, and enter the credentials</li>
                     <li>Make note of the displayed IP address</li>
@@ -85,14 +95,14 @@ class GetStarted extends Component {
                       and password weather
                     </li>
                     <li>Run the following commands:</li>
-                    <ol>
-                      <li>
-                        <pre>cd skypi</pre>
-                      </li>
-                      <li>
-                        <pre>sudo python3 new_station.py</pre>
-                      </li>
-                    </ol>
+                    <ul>
+                        <li>
+                          <pre>cd skypi</pre>
+                          </li>
+                        <li>
+                          <pre>sudo python3 new_station.py</pre>
+                        </li>
+                    </ul>
                     <li>Follow the instructions to finish setup</li>
                   </ol>
                 </div>
@@ -120,30 +130,92 @@ class GetStarted extends Component {
               >
                 <div class="card-body">
                   <ol>
-                    <li>
-                      Ensure that you have Python 3 and Pip for Python 3
-                      installed
+                    <li className="mb-2">Please use the following table to ensure the BME280 sensor is plugged into
+                      the correct GPIO pins
                     </li>
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">Signal</th>
+                          <th scope="col">Raspberry Pi Pin Number</th>
+                          <th scope="col">BME280 Pin</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Power +5V</td>
+                          <td>Pin2</td>
+                          <td>VIN Pin</td>
+                        </tr>
+                        <tr>
+                          <td>Data SDA</td>
+                          <td>Pin 3</td>
+                          <td>SDI Pin</td>
+                        </tr>
+                        <tr>
+                          <td>Clock SCL</td>
+                          <td>Pin 5</td>
+                          <td>SCK Pin</td>
+                        </tr>
+                        <tr>
+                          <td>Ground</td>
+                          <td>Pin 6</td>
+                          <td>GND PIN</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <li>Ensure the BME280 sensor is setup</li>
+                      <ol>
+                        <li>Run the following command
+                          <ul>
+                            <pre>sudo raspi-config</pre>
+                          </ul>
+                          <ul>
+                            <li>Use the down arrow to select 5 Interfacing Options</li>
+                            <li>Arrow down to P5 I2C</li>
+                            <li>Select yes when it asks you to enable I2C</li>
+                            <li>Also select yes if it asks about automatically loading the kernel module</li>
+                            <li>Use the right arrow to select the Finish button</li>
+                            <li>Select yes if/when it asks to reboot</li>
+                          </ul>
+                          <li>The system will reboot. when it comes back up, log in and enter the following command</li>
+                            <ul>
+                              <li><pre>ls /dev/*i2c*</pre></li>
+                            </ul>
+                          <li>The Pi should respond with the following</li>
+                          <ul>
+                            <li><pre>/dev/i2c-1</pre></li>
+                          </ul>
+                          <li>Enter the following command</li>
+                          <ul>
+                            <li><pre>sudo apt-get install -y i2c-tools</pre></li>
+                            <li><pre>sudo i2cdetect -y 1</pre></li>
+                            <li><pre>sudo apt-get update</pre></li>
+                            <li><pre>sudo apt-get install build-essential python-pip python-dev python-smbus git</pre></li>
+                          </ul>
+                        </li>
+                      </ol>
+                    <li>Ensure that you have Python 3 and Pip for Python 3</li>
                     <li>Ensure that Git is installed</li>
-                    <ol>
+                    <ul>
                       <li>
                         <pre>sudo apt install git</pre>
                       </li>
-                    </ol>
+                    </ul>
                     <li>Clone the GitHub repository below:</li>
-                    <ol>
+                    <ul>
                       <li>
                         <pre>
                           git clone https://github.com/alexanderwoodle/skypi
                         </pre>
                       </li>
-                    </ol>
+                    </ul>
                     <li>
                       Enter the following command to change into the SkyPi
                       directory to install the required modules, and register
                       your device
                     </li>
-                    <ol>
+                    <ul>
                       <li>
                         <pre>cd skypi</pre>
                       </li>
@@ -153,7 +225,7 @@ class GetStarted extends Component {
                       <li>
                         <pre>sudo python3 new_station.py</pre>
                       </li>
-                    </ol>
+                    </ul>
                     <li>Follow the instructions to finish setup</li>
                   </ol>
                 </div>
