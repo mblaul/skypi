cbf99dfa3eded14860326262f0c391f3f9df62fd
mblaul <mattblaul@gmail.com>
2019-05-09 23:39:10 -0400

    Cleaning up weather log mutation code

--- backend/api/app/graphql/mutations/create_weather_log.rb
+++ backend/api/app/graphql/mutations/create_weather_log.rb
@@ -2,17 +2,13 @@ module Mutations
   class CreateWeatherLog < BaseMutation
     null true 
     
-    argument :weather_log, Types::WeatherLogInputType, required: true
+    argument :weather_log_input, Types::WeatherLogInputType, required: true
 
     field :weather_log, Types::WeatherLogType, null: false
 
-    def resolve(weather_log: nil)
-      station_id = weather_log[:station_id]
-      temperature = weather_log[:temperature]
-      WeatherLog.create(
-        station_id: station_id,
-        temperature: temperature
-      )
+    def resolve(weather_log_input: nil)
+      weather_log = WeatherLog.create(weather_log_input.to_h)
+      { weather_log: weather_log }
     end
   end
 end
